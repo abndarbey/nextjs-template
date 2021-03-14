85415e57432556f0ad4e8ba81ab0442c9bac90db
Abhinandan Darbey <abhinandan.darbey@gmail.com>
2021-03-14 09:16:33 +0530

    Remove SCSS files

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Navbar from '../Navbar'
 import Copyright from '../Copyright'
 import Footer from '../Footer';
-// import styles from './Layout.module.scss'
 
 const Layout = ({children}) => {
     return (
